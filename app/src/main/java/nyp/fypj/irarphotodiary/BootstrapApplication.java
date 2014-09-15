62c49e5fd8fc6b62b2af62b4d708e307dc01d23d
unknown <L33533@L33533.citi.sit>
2014-09-15 19:04:09 +0800

    Tidy up.

--- app/src/main/java/nyp/fypj/irarphotodiary/BootstrapApplication.java
+++ app/src/main/java/nyp/fypj/irarphotodiary/BootstrapApplication.java
@@ -3,6 +3,8 @@ package nyp.fypj.irarphotodiary;
 import android.app.Application;
 
 import com.cloudinary.Cloudinary;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 
 /**
  * Created by L33533 on 9/12/2014.
@@ -15,7 +17,12 @@ public class BootstrapApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
+        // Cloudinary API Services Initialization
         cloudinary = new Cloudinary(this.getApplicationContext());
+
+        // Android Universal ImageLoder initialization
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(getApplicationContext()).build();
+        ImageLoader.getInstance().init(config);
     }
 
     public Cloudinary getCloudinary() {
---
+++ app/src/main/java/nyp/fypj/irarphotodiary/DashboardHomeFragment.java
@@ -0,0 +1,102 @@
+package nyp.fypj.irarphotodiary;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.etsy.android.grid.StaggeredGridView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DashboardHomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard_home, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        List<String> data = new ArrayList<String>();
+        data.add("January");
+        data.add("February");
+        data.add("March");
+
+        StaggeredGridView staggeredGridView = (StaggeredGridView) getView().findViewById(R.id.dashboardHomeFragmentStaggeredGridView);
+        DashboardHomeFragmentAdapter dashboardHomeFragmentAdapter = new DashboardHomeFragmentAdapter(staggeredGridView.getContext(), data);
+        staggeredGridView.setAdapter(dashboardHomeFragmentAdapter);
+    }
+
+    private class DashboardHomeFragmentAdapter extends BaseAdapter{
+        private LayoutInflater layoutInflater;
+        private List<String> data;
+
+        public DashboardHomeFragmentAdapter(Context context, List<String> data){
+            this.layoutInflater = LayoutInflater.from(context);
+            this.data = data;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return data.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            View view;
+            ViewHolder viewHolder;
+
+            if(convertView == null){
+                view = layoutInflater.inflate(R.layout.adapter_fragment_dashboard_home, parent, false);
+                viewHolder = new ViewHolder();
+                viewHolder.dashboardHomeItemImage = (ImageView) view.findViewById(R.id.dashboardHomeItemImage);
+                viewHolder.dashboardHomeItemTitle = (TextView) view.findViewById(R.id.dashboardHomeItemTitle);
+                viewHolder.dashboardHomeItemDescription = (TextView) view.findViewById(R.id.dashboardHomeItemDescription);
+                view.setTag(viewHolder);
+            }else{
+                view = convertView;
+                viewHolder = (ViewHolder) view.getTag();
+            }
+
+            String datum = data.get(i);
+            viewHolder.dashboardHomeItemTitle.setText(datum);
+            viewHolder.dashboardHomeItemDescription.setText(datum + " (description)");
+            return view;
+        }
+        private class ViewHolder {
+            public ImageView dashboardHomeItemImage;
+            public TextView dashboardHomeItemTitle;
+            public TextView dashboardHomeItemDescription;
+        }
+    }
+
+
+}
