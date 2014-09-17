6be0945bc4bf71a507a47083bfab88974075c528
kfwong <kfwong@zoho.com>
2014-09-17 18:56:27 +0800

    Added color profiling to generate image's dominant colors and palette.

--- app/src/main/java/nyp/fypj/irarphotodiary/SecondFragment.java
+++ app/src/main/java/nyp/fypj/irarphotodiary/SecondFragment.java
@@ -1,28 +1,60 @@
 package nyp.fypj.irarphotodiary;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
-import nyp.fypj.irarphotodiary.R;
+import nyp.fypj.irarphotodiary.util.ColorProfiler;
 
 public class SecondFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.second_frag, container, false);
+        final View v = inflater.inflate(R.layout.second_frag, container, false);
 
-        TextView tv = (TextView) v.findViewById(R.id.tvFragSecond);
+        final ProgressBar progressBar = (ProgressBar) v.findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+
+        final TextView tv = (TextView) v.findViewById(R.id.tvFragSecond);
         tv.setText(getArguments().getString("msg"));
 
         ImageView imageView = (ImageView) v.findViewById(R.id.imageView);
-        ImageLoader.getInstance().displayImage("http://res.cloudinary.com/dxspdhqz3/image/upload/v1410399897/sample.jpg", imageView);
+        ImageLoader.getInstance().displayImage("http://res.cloudinary.com/dxspdhqz3/image/upload/v1410399897/sample.jpg", imageView,new ImageLoadingListener() {
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {
+                progressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(v.getContext(), "Image loading failed.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                progressBar.setVisibility(View.GONE);
+                tv.setText(ColorProfiler.generateJsonProfile(loadedImage));
+                Toast.makeText(v.getContext(), "Image loading completed.", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(v.getContext(), "Image loading cancelled.", Toast.LENGTH_SHORT).show();
+            }
+        });
 
         return v;
     }
