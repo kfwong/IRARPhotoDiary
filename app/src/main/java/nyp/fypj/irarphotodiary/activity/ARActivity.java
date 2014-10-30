c1323626392c5c3a8d323f62e9690fcdfb6740d4
kfwong <kfwong@zoho.com>
2014-10-30 17:46:59 +0800

    Added AR feature. Still need debugging.

--- app/src/main/java/nyp/fypj/irarphotodiary/activity/ARActivity.java
+++ app/src/main/java/nyp/fypj/irarphotodiary/activity/ARActivity.java
@@ -1,15 +1,26 @@
 package nyp.fypj.irarphotodiary.activity;
 
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.beyondar.android.fragment.BeyondarFragmentSupport;
+import com.beyondar.android.util.ImageUtils;
+import com.beyondar.android.view.BeyondarViewAdapter;
+import com.beyondar.android.view.OnClickBeyondarObjectListener;
+import com.beyondar.android.world.BeyondarObject;
+import com.beyondar.android.world.BeyondarObjectList;
 import com.beyondar.android.world.GeoObject;
 import com.beyondar.android.world.World;
 import com.nostra13.universalimageloader.core.DisplayImageOptions;
@@ -18,7 +29,12 @@ import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
 import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import nyp.fypj.irarphotodiary.R;
@@ -29,7 +45,6 @@ public class ARActivity extends FragmentActivity {
 
     private BeyondarFragmentSupport mBeyondarFragment;
     private World mWorld;
-
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,46 +52,36 @@ public class ARActivity extends FragmentActivity {
 
         // Hide the window title.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-
         setContentView(R.layout.activity_ar);
-
-        // Get imageProfiles from intent
-        ArrayList<ImageProfile> imageProfiles = getIntent().getParcelableArrayListExtra("imageProfiles");
-
         mBeyondarFragment = (BeyondarFragmentSupport) getSupportFragmentManager().findFragmentById(
                 R.id.beyondarFragment);
-
         // We create the world and fill it ...
-        mWorld = new World(this);
-
-        // The user can set the default bitmap. This is useful if you are
-        // loading images form Internet and the connection get lost
-        mWorld.setDefaultImage(R.drawable.placeholder);
-
         // User position (you can change it using the GPS listeners form Android
         // API)
+        mWorld = new World(this);
         mWorld.setGeoPosition(1.37912487, 103.84926296);
+
+        mWorld.setDefaultImage(R.drawable.placeholder);
+
+        ArrayList<ImageProfile> imageProfiles = getIntent().getParcelableArrayListExtra("imageProfiles");
 
         // Loop through each imageProfiles, create GeoObjects
         for(ImageProfile imageProfile : imageProfiles){
             GeoObject geoObject = new GeoObject();
             geoObject.setGeoPosition(imageProfile.getLatitude(), imageProfile.getLongitude());
             geoObject.setName(imageProfile.getTitle());
-
-            Log.e("TADAH","imageProfile:"+imageProfile.getTitle()+":"+imageProfile.getLatitude()+":"+imageProfile.getLongitude());
-
-            //Bitmap loadedImage = ImageLoader.getInstance().loadImageSync("http://res.cloudinary.com/" + BootstrapApplication.CLOUDINARY_CLOUD_NAME + "/image/upload/w_0.1/" + imageProfile.getFilename() + "." + imageProfile.getExtension(), tinySize);
-            //geoObject.setImageUri("http://res.cloudinary.com/" + BootstrapApplication.CLOUDINARY_CLOUD_NAME + "/image/upload/w_100,h_100,c_thumb/w_250,h_100,c_pad,g_south_west,bo_3px_solid_rgb:00000090,b_rgb:3399aa/l_text:arial_11_bold_underline:Lorem ipsum,g_north_west,x_113,y_15,c_fit,w_130,h_65,co_white/l_text:arial_11:Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.,g_north_west,x_113,y_35,c_fit,w_130,h_65,co_white/" + imageProfile.getFilename() + "." + imageProfile.getExtension());
-            //geoObject.setImageResource(R.drawable.placeholder);
-            geoObject.setImageUri("http://res.cloudinary.com/dxspdhqz3/image/upload/w_100,h_100,c_thumb/w_250,h_100,c_pad,g_south_west,bo_3px_solid_rgb:00000090,b_rgb:3399aa/mkd1fftrv89qsxxbejuj.jpg");
+            //WORKING: geoObject.setImageUri("http://res.cloudinary.com/" + BootstrapApplication.CLOUDINARY_CLOUD_NAME + "/image/upload/w_512,h_512,c_thumb/" + imageProfile.getFilename() + "." + imageProfile.getExtension());
+            //PARTIAL WORKING: geoObject.setImageUri("http://res.cloudinary.com/dxspdhqz3/image/upload/w_512,h_512,c_thumb/w_348,h_128,c_pad,g_south_west,bo_5px_solid_rgb:00000090,b_rgb:000000/l_text:arial_18_bold_underline:Title,g_north_west,x_138,y_15,c_fit,w_192,h_65,co_white/l_text:arial_16:Description.,g_north_west,x_138,y_40,c_fit,w_192,h_95,co_white/sfls6bydbraqv4im43n0.jpg");
+            geoObject.setImageUri("http://res.cloudinary.com/"+BootstrapApplication.CLOUDINARY_CLOUD_NAME+"/image/upload/w_512,h_512,c_thumb,bo_50px_solid_rgb:33b5e5/"+imageProfile.getFilename()+"."+imageProfile.getExtension());
 
             // Add the GeoObjects to the world
             mWorld.addBeyondarObject(geoObject, 1);
         }
-
         // ... and send it to the fragment
         mBeyondarFragment.setWorld(mWorld);
-
+        // We also can see the Frames per seconds
+        //mBeyondarFragment.showFPS(true);
     }
+
 
 }
